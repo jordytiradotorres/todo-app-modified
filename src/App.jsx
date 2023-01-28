121cf7566821e787bc97226898623a151cc55310
Jordy Tirado Torres <jordytiradotorres20@gmail.com>
2023-01-28 08:01:20 -0500

    feat(App): eliminando el archivo app.css y logo react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import "./scss/app.scss";
 
 function App() {
   const [count, setCount] = useState(0);
