ed029d2b9b4ba2e9d979b2cbf1dfca11f14fcfb8
Jordy Tirado Torres <jordytiradotorres20@gmail.com>
2023-01-28 07:38:04 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      {/* Todo
+      <!-- Add dynamic number --> items left
+
+      All
+      Active 
+      Completed
+
+      Clear Completed
+
+      Drag and drop to reorder list */}
+      <div class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </div>
+  );
+}
+
+export default App;
