9f911cd7c213816128f5c7329796920c7feec9bd
Jordy Tirado Torres <jordytiradotorres20@gmail.com>
2023-02-08 18:59:04 -0500

    feat: agregue prop-types a todos los componentes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,9 @@
+import PropTypes from "prop-types";
+
 export const Footer = ({ text = "" }) => {
   return <footer className="todo-footer">{text}</footer>;
+};
+
+Footer.propTypes = {
+  text: PropTypes.string.isRequired,
 };
